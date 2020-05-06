2796095f0edd50f11e141c16ace13b5ad2dba796
beHungry <nothing>
2020-05-07 00:17:30 +0800

    learning

---
+++ src/test/java/com/goku/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goku.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
